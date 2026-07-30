bae0214353b9e0f3c088d91fc233ad78caf9c66d
Daniel Niedergesäß <daniel.niedergesaess@gmail.com>
2026-07-30 23:33:45 +0200

    chore: trigger UI build

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 
+// Build the HACS frontend bundle from the TypeScript source.
 export default {
   input: "src/plant-climate-card.ts",
   output: {
